b24f0b4925561e6db9e73023dace7b239a6cd9d7
Nestorsoiza <nestor.soiza83@gmail.com>
2020-11-30 19:20:55 +0100

    Implemento el main y voy haciendo pruebas

---
+++ src/ecuaciones/Principal.java
@@ -0,0 +1,15 @@
+package ecuaciones;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Raices ecuacion = new Raices(1, 4, 4);
+		ecuacion.calcular();
+		Raices ecuacion2 = new Raices(8, 4, 4);
+		ecuacion2.calcular();
+		Raices ecuacion3 = new Raices(2, 8, 2);
+		ecuacion3.calcular();
+		ecuacion3.toString();
+	}
+
+}
--- src/ecuaciones/Raices.java
+++ src/ecuaciones/Raices.java
@@ -1,5 +1,47 @@
 package ecuaciones;
 
 public class Raices {
+	private double a;
+	private double b;
+	private double c;
+	
+	public Raices(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 
+	private void obtenerRaices() {
+		double x1 = (-b + Math.sqrt(getDiscriminante()))/(2*a);
+		double x2 = (-b - Math.sqrt(getDiscriminante()))/(2*a);
+		System.out.println("Solucion x1: " + x1);
+		System.out.println("Solucion x2: " + x2);
+
+	}
+	
+	private void obtenerRaiz() {
+		double x = (-b) / (2 * a);
+		System.out.println(" Unica solución " + x);
+	}
+
+	private double getDiscriminante() {
+		return Math.pow(b, 2)-(4*a*c);
+	}
+	
+	private boolean tieneRaices() {
+		return getDiscriminante() > 0;
+	}
+	
+	private boolean tieneRaiz() {
+		return getDiscriminante() == 0;
+	}
+	public void calcular() {
+		if (tieneRaices()) {
+			obtenerRaices();
+		} else if (tieneRaiz()) {
+			obtenerRaiz();
+		}else {
+			System.out.println("No tiene soluciones");
+		}
+	}
 }
